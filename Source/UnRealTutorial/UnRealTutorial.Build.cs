4779dfefe2a8194e2fd342e2f8807c657937949f
Youssef Hanna <yhanna@coreworld.local>
2020-09-02 16:49:35 +0200

    Adding UMG and Salte and SalteCore Dependencies

--- Source/UnRealTutorial/UnRealTutorial.Build.cs
+++ Source/UnRealTutorial/UnRealTutorial.Build.cs
@@ -8,6 +8,9 @@ public class UnRealTutorial : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" , "UMG" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
 	}
 }
